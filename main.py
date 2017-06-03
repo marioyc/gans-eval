90c0a2e9ee4ab054f22001231c235f100bbbdaad
marioyc <ycmario@gmail.com>
2017-06-03 07:39:56 +0200

    change default params

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from gan import GAN
 from wgan import WGAN
 
 params = {
-    'z_dim': 2,
+    'z_dim': 64,
     'data': {
         'n_mixture': 8,
-        'std': 0.01,
+        'std': 0.1,
         'radius': 1,
     },
     'generator': {
@@ -25,13 +25,13 @@ params = {
 }
 
 wgan = WGAN(params)
-wgan.train(iterations=10000, discriminator_steps=5, batch_size=256, visualization_step=500)
+wgan.train(iterations=10000, discriminator_steps=5, batch_size=256, visualization_step=2500)
 
 params = {
-    'z_dim': 2,
+    'z_dim': 64,
     'data': {
         'n_mixture': 8,
-        'std': 0.01,
+        'std': 0.1,
         'radius': 1,
     },
     'generator': {
@@ -53,4 +53,4 @@ params = {
 
 tf.reset_default_graph()
 gan = GAN(params)
-gan.train(iterations=10000, batch_size=256, visualization_step=500)
+gan.train(iterations=10000, batch_size=256, visualization_step=2500)
